ab17b3316f5fb9d81dadea410d8eb3d24b26a86b
Aleksandr Markelov <25wtf.marcus@gmail.com>
2021-08-06 00:34:41 +0300

    Fields validation, Input component

--- src/components/Header/Login/Login.js
+++ src/components/Header/Login/Login.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { API_URL, API_KEY_3, fetchApi } from '../../../utils/api';
 import { Modal, ModalBody } from 'reactstrap';
 import LoginForm from './LoginForm.js';
 
@@ -15,48 +14,6 @@ export default class Login extends React.Component {
     this.setState((prevState) => ({
       showModal: !prevState.showModal,
     }));
-  };
-
-  sendPromises = async () => {
-    try {
-      const data = await fetchApi(
-        `${API_URL}/authentication/token/new?api_key=${API_KEY_3}`
-      );
-
-      const result = await fetchApi(
-        `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`,
-        {
-          method: 'POST',
-          mode: 'cors',
-          headers: {
-            'Content-type': 'application/json',
-          },
-          body: JSON.stringify({
-            username: 'babilunga1',
-            password: 'dafadurka',
-            request_token: data.request_token,
-          }),
-        }
-      );
-
-      const { session_id } = await fetchApi(
-        `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`,
-        {
-          method: 'POST',
-          mode: 'cors',
-          headers: {
-            'Content-type': 'application/json',
-          },
-          body: JSON.stringify({
-            request_token: result.request_token,
-          }),
-        }
-      );
-
-      console.log('session_id:', session_id);
-    } catch (error) {
-      console.log('error:', error);
-    }
   };
 
   render() {
--- src/components/Header/Login/LoginForm.js
+++ src/components/Header/Login/LoginForm.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { API_URL, API_KEY_3, fetchApi } from '../../../utils/api';
+import Input from './../../UIComponents/Input.js';
 
 export default class LoginForm extends React.Component {
   constructor() {
@@ -6,80 +8,176 @@ export default class LoginForm extends React.Component {
     this.state = {
       username: '',
       password: '',
+      repeatPassword: '',
       errors: {},
     };
   }
 
-  onChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    console.log(name, '>', value);
-    this.updateValues(name, value);
+  getErrors = (target = null) => {
+    const { username, password, repeatPassword } = this.state;
+    const errorsCollector = {};
+
+    // Errors Info Container
+    const errors = {
+      username: {
+        test: username.length === 0,
+        label: '*Required field',
+      },
+      password: {
+        test: password.length === 0,
+        label: '*Required field',
+      },
+      repeatPassword: {
+        test: repeatPassword.length === 0 || repeatPassword !== password,
+        label: '*Should be equal to password',
+      },
+      // base: {},
+    };
+
+    // Errors Handling
+    //    As single form error (when onBlur)
+    if (target !== null) {
+      errorsCollector[target] = errors[target].test
+        ? errors[target].label
+        : null;
+    }
+    //    As all fields (when onSubmit)
+    else {
+      Object.entries(errors).map(([key, value]) => {
+        errorsCollector[key] = value.test ? value.label : null;
+      });
+    }
+    console.log(errorsCollector);
+    return errorsCollector;
   };
 
-  updateValues = (name, value) => {
-    this.setState({
-      [name]: value,
-    });
+  onChange = (e) => {
+    const params = {
+      name: e.target.name,
+      value: e.target.value,
+      errors_value: { [e.target.name]: null },
+    };
+    this.updateValues(params);
+  };
+
+  updateValues = ({ name = null, value = null, errors_value = null }) => {
+    // Check does we need to update other fields, or only Errors
+    const nameValue = name !== null ? { [name]: value } : {};
+    this.setState((prevState) => ({
+      ...nameValue,
+      errors: {
+        ...prevState.errors,
+        ...errors_value,
+      },
+    }));
   };
 
   handleBlur = (e) => {
-    console.log('on blur:', e.target.name);
-  };
-
-  getErrors = () => {
-    const errors = {};
-
-    if (this.state.username.length === 0) {
-      errors.username = '*Required field';
+    const errors = this.getErrors(e.target.name);
+    if (Object.keys(errors).length > 0) {
+      this.updateValues({ errors_value: errors });
     }
-    if (this.state.password.length === 0) {
-      errors.password = '*Required field';
-    }
-
-    return errors;
   };
 
   onLogin = (e) => {
     e.preventDefault();
 
-    const errors = this.getErrors;
+    const errors = this.getErrors();
+
+    // if error equals null - it means it was resolved and it is no longer an error
+    const realErrors = Object.values(errors).filter((error) => error !== null);
+
+    if (realErrors.length > 0) {
+      console.log('Unsuccess!');
+      this.updateValues({ errors_value: errors });
+    } else {
+      console.log('Success!');
+    }
+  };
+
+  onSubmit = async () => {
+    try {
+      const data = await fetchApi(
+        `${API_URL}/authentication/token/new?api_key=${API_KEY_3}`
+      );
+
+      const result = await fetchApi(
+        `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`,
+        {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            username: this.state.username,
+            password: this.state.password,
+            request_token: data.request_token,
+          }),
+        }
+      );
+
+      const { session_id } = await fetchApi(
+        `${API_URL}/authentication/session/new?api_key=${API_KEY_3}`,
+        {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify({
+            request_token: result.request_token,
+          }),
+        }
+      );
+      console.log('session_id:', session_id);
+    } catch (error) {
+      console.log('error:', error);
+    }
   };
 
   render() {
-    const { username, password, errors } = this.state;
+    const { username, password, repeatPassword, errors } = this.state;
     return (
       <div>
         <h1 className="text-center h2">Authorization</h1>
         <div className="mb-3">
-          <label htmlFor="username" className="form-label">
-            Username
-          </label>
-          <input
+          <Input
+            label="Username"
             type="text"
-            className="form-control"
             id="username"
             name="username"
             value={username}
-            onChange={this.onChange}
             placeholder="Enter your username"
-            onBlur={this.handleBlur}
+            onChange={this.onChange}
+            handleBlur={this.handleBlur}
+            errors={errors}
           />
-          {errors && <div className="invalid-feedback">{errors.username}</div>}
         </div>
-        <div className="mb-3">
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            type="text"
-            className="form-control"
+        <div className="mb-3 border-red">
+          <Input
+            label="Password"
+            type="password"
             id="password"
             name="password"
             value={password}
-            onChange={this.onChange}
             placeholder="Enter your password"
-            onBlur={this.handleBlur}
+            onChange={this.onChange}
+            handleBlur={this.handleBlur}
+            errors={errors}
+          />
+        </div>
+        <div className="mb-3">
+          <Input
+            label="Repeat password"
+            type="password"
+            id="repeatPassword"
+            name="repeatPassword"
+            value={repeatPassword}
+            placeholder="Repeat password"
+            onChange={this.onChange}
+            handleBlur={this.handleBlur}
+            errors={errors}
           />
         </div>
         <button
